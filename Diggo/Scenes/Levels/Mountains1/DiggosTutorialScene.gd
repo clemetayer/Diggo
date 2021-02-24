7aec5e3e9ffbdbdf676ce7fb717eec4344fa728b
Corentin Le Métayer <corentinlm@corentin-vivobookasuslaptopx570zd>
2021-02-24 20:46:35 +0100

    Started tutorial scene

---
+++ Diggo/Scenes/Levels/Mountains1/DiggosTutorialScene.gd
@@ -0,0 +1,21 @@
+extends Node2D
+
+# TODO : fix a bit the tilemap looking a bit weird on some spots
+# TODO : add warning panels in front of the cliff (add another panel each time the player jumps off the cliff)
+# TODO : add a Game over transition if the player jumps off the cliff
+# TODO : add an ice or rock wall in the tunnel to prevent the player to follow the story too fast
+# TODO : Game over screen + different texts each time the player jumps off the cliff
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
