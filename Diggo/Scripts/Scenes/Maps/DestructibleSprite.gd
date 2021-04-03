b3084c1dc0ef1e1f68c470bb8d999d4129b051ca
clemetayer <c.lemetayer@laposte.net>
2021-04-03 15:20:37 +0200

    Reverted DestructibleTilemap to a less optimized, but working version (and also removed ViewportSpy plugin files)

--- Diggo/Scripts/Scenes/Maps/DestructibleSprite.gd
+++ Diggo/Scripts/Scenes/Maps/DestructibleSprite.gd
@@ -1,8 +1,5 @@
 extends Node2D
 
-# TODO : check with a sprite with odd size
-# OPTIMIZATION : maybe use a polygon collision shape (i don"t know if that would improve things)
-
 # Here is how it works
 # first, the sprite is converted to a bitmap, to detect the transparent and not transparent parts
 # then, one divide this sprite in max size blocks (2^TILE_SIZE_POW)
@@ -10,45 +7,53 @@ extends Node2D
 # else if this block is completely full, then one put a destructible block in it
 # else if it is not entirely full/transparent, one divide the block in 4, and redo the if-else section for each block
 
-export var TILE_SIZE_POW = 6 # Starting tile size (as a power of 2, i.e 2^6 = 64)
+signal destructible_sprite_loaded()
+
+export var TILE_SIZE_POW = 8 # Starting tile size (as a power of 2, i.e 2^6 = 64)
 export var MIN_SIZE_POW = 2 # Minimal tile size (as a power of 2, i.e 2^2 = 4)
 export var MIN_FPS = 15 # Minimal goal FPS
 export var GOAL_FPS = 30 # Desired average FPS
 
 export(Texture) var SPRITE # General texture of the destructible block (only non transparent part will have a Destructible block)
-export(String) var BLOCK_PATH = "res://Scenes/Utils/Blocks/BlockArea.tscn" # Path of an instance of BlockDestructible
+export(String) var BLOCK_PATH = "res://Scenes/Utils/Blocks/BlockArea.tscn"# Path of an instance of BlockDestructible
 																		   # By default, it is a BlockArea, but you can also set it up to "BlockPysics" if you want to toast your CPU
 
 var stepFPS = (GOAL_FPS - MIN_FPS)/3 # Number of "steps" of FPS states (ex: 60-40-20)
-var stepPow = int(TILE_SIZE_POW - MIN_SIZE_POW/3)
 var blockArea # load of BLOCK_PATH
 var bitmap = BitMap # bitmap of the SPRITE
 var minSize = pow(2,MIN_SIZE_POW) # true value of minimal size (2^MIN_SIZE_POW)
+var started = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	started = false
+
+func parseSprite():
 	blockArea = load(BLOCK_PATH)
 	bitmap = BitMap.new()
 	bitmap.create_from_image_alpha(SPRITE.get_data())
 	subdivideOriginalSprite()
+	started = true
+	emit_signal("destructible_sprite_loaded")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
-	checkFPS()
+	if(started):
+		checkFPS()
 
 # Check FPS, and changes the minimal block size if it is too low
 func checkFPS():
 	var avgFps = Performance.get_monitor(Performance.TIME_FPS)
 	if(avgFps > GOAL_FPS):
-		minSize = pow(2,MIN_SIZE_POW)
+		minSize = 4
 	elif((avgFps < GOAL_FPS) and (avgFps >= MIN_FPS + 2*stepFPS)):
-		minSize = pow(2,MIN_SIZE_POW + stepPow)
+		minSize = 8
 	elif((avgFps < MIN_FPS + 2*stepFPS) and (avgFps >= MIN_FPS + stepFPS)):
-		minSize = pow(2,MIN_SIZE_POW + 2 * stepPow)
+		minSize = 16
 	elif((avgFps < MIN_FPS + stepFPS) and (avgFps >= MIN_FPS)):
-		minSize = pow(2,MIN_SIZE_POW + 3 * stepPow)
+		minSize = 32
 	else:
-		minSize = pow(2,MIN_SIZE_POW + 4 * stepPow)
+		minSize = 64
 
 # Returns an array : ret[0] => is empty; ret[1] => is full
 func isTransparentOrFull(size, position):
--- Diggo/Scripts/Scenes/Maps/DestructibleTilemapBkp.gd
+++ Diggo/Scripts/Scenes/Maps/DestructibleTilemapBkp.gd
@@ -1,24 +1,27 @@
 extends TileMap
 
 export(String,FILE) var DESTRUCTIBLE_SPRITE_PATH = "res://Scenes/Maps/DestructibleSprite.tscn"
-export(String,FILE) var BLOCK_PATH = "res://Scenes/Utils/Blocks/BlockArea.tscn"
-export(int) var TILE_SIZE_POW = 8
+export(String,FILE) var BLOCK_PATH = "res://Scenes/Utils/Blocks/BlockArea.tscn" 
+export(int) var TILE_SIZE_POW = 6 
 export(int) var MIN_SIZE_POW = 2
 
 var viewport
 
 signal destructible_loaded()
 
+# FIXME : Not working sometimes, i don't know why
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	yield(VisualServer, 'frame_post_draw') # waits one frame before getting the texture, otherwise, some unexpected behaviours happens
 	var TileMapRect = computeTileMapBounds()
-	var TileMapPosition = position
+	var TileMapPosition = position + TileMapRect.position 
 	viewport = createViewport(TileMapPosition,TileMapRect.size)
 	addTilemapToViewport(TileMapRect)
-	yield(VisualServer, 'frame_post_draw') # waits one frame before getting the texture, otherwise, some unexpected behaviours happens
 	# Note : TileMapRect.position is the offset from the center position to the first pixel on the top left
 	createDestructibleSprite(TileMapRect.position)
-	get_parent().get_parent().get_parent().remove_child(get_parent().get_parent())
+	self.get_parent().get_parent().get_parent().remove_child(get_parent().get_parent())
+	emit_signal("destructible_loaded")
 #	get_parent().get_parent().call_deferred("queue_free") # FIXME : The texture of the viewport is linked in memory to the one in the sprite, so freeing the viewport will free the texture
 
 # creates the destructible sprite corresponding to the viewport of the tilemap
@@ -26,22 +29,22 @@ func createDestructibleSprite(pos):
 	var destructible_instance = load(DESTRUCTIBLE_SPRITE_PATH).instance()
 	var img = viewport.get_texture()
 	img.get_data().flip_y()
-#	img.get_data().save_png("res://test.png") # debug
-	destructible_instance.position = pos
+	img.get_data().save_png("res://test.png") # debug
+	destructible_instance.position = pos 
 	destructible_instance.SPRITE = img
 	destructible_instance.BLOCK_PATH = BLOCK_PATH
 	destructible_instance.TILE_SIZE_POW = TILE_SIZE_POW
 	destructible_instance.MIN_SIZE_POW = MIN_SIZE_POW
 	get_parent().get_parent().get_parent().add_child(destructible_instance)
-	destructible_instance.connect("destructible_sprite_loaded",self,"destructibleLoaded")
-	destructible_instance.parseSprite()
+#	get_parent().get_parent().get_parent().call_deferred("add_child",destructible_instance)
 
 # adds tilemap as a child of the viewport
 func addTilemapToViewport(tilemapRect):
-	get_parent().call_deferred("remove_child",self)
-	viewport.call_deferred("add_child", self)
+	get_parent().remove_child(self)
+	viewport.add_child(self) # FIXME : issue here is that it does the rest BEFORE adding child. Fix with a method waiting for child to be added
+							 #         Oh, and some position issues also
 	# offsets the tilemap position to the first pixel on the top left
-	position = Vector2(0,0) - tilemapRect.position
+	position = Vector2(0,0)
 
 # function by CKO on godot forum : https://godotengine.org/qa/3276/tilemap-size
 func computeTileMapBounds():
@@ -54,15 +57,12 @@ func computeTileMapBounds():
 func createViewport(position,size):
 	var viewportC = ViewportContainer.new()
 	viewportC.rect_size = size
-	viewportC.rect_position = position
+	viewportC.rect_position = position - size/2
 	viewport = Viewport.new()
 	viewport.transparent_bg = true
 	viewport.size = size
 	viewport.set_usage(0)
 	viewport.render_target_v_flip = true
 	viewportC.add_child(viewport)
-	get_parent().call_deferred("add_child",viewportC)
+	get_parent().add_child(viewportC)
 	return viewport
-
-func destructibleLoaded():
-	emit_signal("destructible_loaded")
